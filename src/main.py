1b2b38f1298b0003c19fda3903b79cb9bbce8778
hiab <tets2@gnail.com>
2025-11-27 04:58:12 -0800

    add say function

--- src/main.py
+++ src/main.py
@@ -1,13 +1,21 @@
 # src/main.py
 from utils import add, multiply, greet
 
+def say(word):
+    print(f"{word}")
+
 def main():
     print("=== Python Project Starter ===")
     name = input("Enter your name: ")
+    word = input('what should i say ')
+    say(word)
     print(greet(name))
 
     a = int(input("Enter first number: "))
     b = int(input("Enter second number: "))
+
+
+
 
     print(f"{a} + {b} = {add(a, b)}")
     print(f"{a} * {b} = {multiply(a, b)}")
